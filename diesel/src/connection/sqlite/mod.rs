6d3a781ec4c1764aeb5967dfd6b9a077a63d4ea0
Sean Griffin <sean@seantheprogrammer.com>
2016-01-23 18:51:57 -0700

    WIP

--- diesel/src/connection/sqlite/mod.rs
+++ diesel/src/connection/sqlite/mod.rs
@@ -1,27 +1,20 @@
-extern crate libsqlite3_sys as ffi;
-extern crate libc;
-
-use std::ptr;
-use std::ffi::{CString, CStr};
+#[doc(hidden)]
+pub mod raw;
 
 use backend::Sqlite;
-use expression::{AsExpression, NonAggregate};
-use expression::helper_types::AsExpr;
-use helper_types::{FindBy, Limit};
 use query_builder::*;
-use query_dsl::*;
 use query_source::*;
 use result::*;
-use result::Error::DatabaseError;
-use super::{SimpleConnection, Connection, PkType, FindPredicate};
+use self::raw::*;
+use super::{SimpleConnection, Connection};
 
 pub struct SqliteConnection {
-    internal_connection: *mut ffi::sqlite3,
+    raw_connection: RawConnection,
 }
 
 impl SimpleConnection for SqliteConnection {
-    fn batch_execute(&self, _query: &str) -> QueryResult<()> {
-        unimplemented!()
+    fn batch_execute(&self, query: &str) -> QueryResult<()> {
+        self.raw_connection.exec(query)
     }
 }
 
@@ -29,111 +22,49 @@ impl Connection for SqliteConnection {
     type Backend = Sqlite;
 
     fn establish(database_url: &str) -> ConnectionResult<Self> {
-        let mut conn_pointer = ptr::null_mut();
-        let database_url = try!(CString::new(database_url));
-        let connection_status = unsafe {
-            ffi::sqlite3_open(database_url.as_ptr(), &mut conn_pointer)
-        };
-
-        match connection_status {
-            ffi::SQLITE_OK => Ok(SqliteConnection {
-                internal_connection: conn_pointer,
-            }),
-            err_code => {
-                let message = error_message(err_code);
-                Err(ConnectionError::BadConnection(message.into()))
+        RawConnection::establish(database_url).map(|conn| {
+            SqliteConnection {
+                raw_connection: conn,
             }
-        }
-    }
-
-    fn transaction<T, E, F>(&self, _f: F) -> TransactionResult<T, E> where
-        F: FnOnce() -> Result<T, E>,
-    {
-        unimplemented!()
-    }
-
-    fn begin_test_transaction(&self) -> QueryResult<usize> {
-        unimplemented!()
-    }
-
-    fn test_transaction<T, E, F>(&self, _f: F) -> T where
-        F: FnOnce() -> Result<T, E>,
-    {
-        unimplemented!()
+        })
     }
 
     fn execute(&self, query: &str) -> QueryResult<usize> {
-        let mut stmt = ptr::null_mut();
-        let mut unused = ptr::null();
-        let query_size = query.len();
-        let query = try!(CString::new(query));
-        let stmt_result = unsafe {
-            ffi::sqlite3_prepare_v2(
-                self.internal_connection,
-                query.as_ptr(),
-                query_size as libc::c_int,
-                &mut stmt,
-                &mut unused,
-            )
-        };
-
-        if stmt_result != ffi::SQLITE_OK {
-            return Err(DatabaseError(last_error_message(self.internal_connection).into()));
-        }
-
-        let result_code = unsafe { ffi::sqlite3_step(stmt) };
-        match result_code {
-            ffi::SQLITE_DONE => Ok(0),
-            _ => Err(DatabaseError(last_error_message(self.internal_connection).into())),
-        }
-    }
-
-    fn query_one<T, U>(&self, _source: T) -> QueryResult<U> where
-        T: AsQuery,
-        T::Query: QueryFragment<Self::Backend>,
-        U: Queryable<T::SqlType>,
-    {
-        unimplemented!()
+        try!(self.batch_execute(query));
+        Ok(self.raw_connection.rows_affected_by_last_query())
     }
 
     fn query_all<'a, T, U: 'a>(&self, _source: T) -> QueryResult<Box<Iterator<Item=U> + 'a>> where
         T: AsQuery,
         T::Query: QueryFragment<Self::Backend>,
-        U: Queryable<T::SqlType>,
+        U: Queryable<T::SqlType, Self::Backend>,
     {
         unimplemented!()
     }
 
-    fn find<T, U, PK>(&self, source: T, id: PK) -> QueryResult<U> where
-        T: Table + FilterDsl<FindPredicate<T, PK>>,
-        FindBy<T, T::PrimaryKey, PK>: LimitDsl,
-        Limit<FindBy<T, T::PrimaryKey, PK>>: QueryFragment<Self::Backend>,
-        U: Queryable<<Limit<FindBy<T, T::PrimaryKey, PK>> as Query>::SqlType>,
-        PK: AsExpression<PkType<T>>,
-        AsExpr<PK, T::PrimaryKey>: NonAggregate,
-    {
-        unimplemented!()
-    }
-
-    fn execute_returning_count<T>(&self, source: &T) -> QueryResult<usize> where
+    fn execute_returning_count<T>(&self, _source: &T) -> QueryResult<usize> where
         T: QueryFragment<Self::Backend>,
     {
         unimplemented!()
     }
 
-    fn silence_notices<F: FnOnce() -> T, T>(&self, f: F) -> T {
+    fn silence_notices<F: FnOnce() -> T, T>(&self, _f: F) -> T {
         unimplemented!()
     }
-}
 
-fn error_message(err_code: libc::c_int) -> &'static str {
-    let message_ptr = unsafe { ffi::sqlite3_errstr(err_code) };
-    let result = unsafe { CStr::from_ptr(message_ptr) };
-    result.to_str().unwrap()
-}
+    fn begin_transaction(&self) -> QueryResult<()>{
+        unimplemented!()
+    }
 
-fn last_error_message(conn: *mut ffi::sqlite3) -> &'static str {
-    let message_ptr = unsafe { ffi::sqlite3_errmsg(conn as *mut ffi::sqlite3) };
-    let result = unsafe { CStr::from_ptr(message_ptr) };
-    result.to_str().unwrap()
+    fn rollback_transaction(&self) -> QueryResult<()> {
+        unimplemented!()
+    }
+
+    fn commit_transaction(&self) -> QueryResult<()> {
+        unimplemented!()
+    }
+
+    fn get_transaction_depth(&self) -> i32 {
+        unimplemented!()
+    }
 }
---
+++ diesel/src/connection/sqlite/raw.rs
@@ -0,0 +1,80 @@
+extern crate libsqlite3_sys as ffi;
+extern crate libc;
+
+use std::ffi::{CString, CStr};
+use std::{ptr, str};
+
+use result::*;
+use result::Error::DatabaseError;
+
+pub struct RawConnection {
+    internal_connection: *mut ffi::sqlite3,
+}
+
+impl RawConnection {
+    pub fn establish(database_url: &str) -> ConnectionResult<Self> {
+        let mut conn_pointer = ptr::null_mut();
+        let database_url = try!(CString::new(database_url));
+        let connection_status = unsafe {
+            ffi::sqlite3_open(database_url.as_ptr(), &mut conn_pointer)
+        };
+
+        match connection_status {
+            ffi::SQLITE_OK => Ok(RawConnection {
+                internal_connection: conn_pointer,
+            }),
+            err_code => {
+                let message = error_message(err_code);
+                Err(ConnectionError::BadConnection(message.into()))
+            }
+        }
+    }
+
+    pub fn exec(&self, query: &str) -> QueryResult<()> {
+        let mut err_msg = ptr::null_mut();
+        let query = try!(CString::new(query));
+        unsafe {
+            ffi::sqlite3_exec(
+                self.internal_connection,
+                query.as_ptr(),
+                None,
+                ptr::null_mut(),
+                &mut err_msg,
+            );
+        }
+
+        if !err_msg.is_null() {
+            let msg = unsafe {
+                let bytes = CStr::from_ptr(err_msg).to_bytes();
+                str::from_utf8_unchecked(bytes).into()
+            };
+            unsafe { ffi::sqlite3_free(err_msg as *mut libc::c_void) };
+            Err(DatabaseError(msg))
+        } else {
+            Ok(())
+        }
+    }
+
+    pub fn rows_affected_by_last_query(&self) -> usize {
+        unsafe { ffi::sqlite3_changes(self.internal_connection) as usize }
+    }
+}
+
+impl Drop for RawConnection {
+    fn drop(&mut self) {
+        let close_result = unsafe { ffi::sqlite3_close(self.internal_connection) };
+        assert_eq!(ffi::SQLITE_OK, close_result);
+    }
+}
+
+fn error_message(err_code: libc::c_int) -> &'static str {
+    let message_ptr = unsafe { ffi::sqlite3_errstr(err_code) };
+    let result = unsafe { CStr::from_ptr(message_ptr) };
+    result.to_str().unwrap()
+}
+
+// fn last_error_message(conn: *mut ffi::sqlite3) -> &'static str {
+//     let message_ptr = unsafe { ffi::sqlite3_errmsg(conn as *mut ffi::sqlite3) };
+//     let result = unsafe { CStr::from_ptr(message_ptr) };
+//     result.to_str().unwrap()
+// }
