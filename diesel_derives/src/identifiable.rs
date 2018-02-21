6bbb9b5bb59ea7fe63891477dfa9e20c2d1e1e36
Georg Semmler <georg_semmler_05@web.de>
2018-02-21 16:14:10 +0100

    Derive `Identifiable` similar to `Insertable` and `AsChangeset`

--- diesel_derives/src/identifiable.rs
+++ diesel_derives/src/identifiable.rs
@@ -14,12 +14,14 @@ pub fn derive(item: syn::DeriveInput) -> Result<quote::Tokens, Diagnostic> {
     ref_generics.params.push(parse_quote!('ident));
     let (ref_generics, ..) = ref_generics.split_for_impl();
 
-    let (field_ty, field_access): (Vec<_>, Vec<_>) = model
+    let (ref_field_ty, ref_field_access): (Vec<_>, Vec<_>) = model
         .primary_key_names
         .iter()
         .filter_map(|&pk| model.find_column(pk).emit_error())
         .map(|f| (&f.ty, f.name.access()))
         .unzip();
+    let direct_field_ty = ref_field_ty.clone();
+    let direct_field_access = ref_field_access.clone();
 
     Ok(wrap_in_dummy_mod(
         model.dummy_mod_name("identifiable"),
@@ -39,10 +41,20 @@ pub fn derive(item: syn::DeriveInput) -> Result<quote::Tokens, Diagnostic> {
             impl #ref_generics Identifiable for &'ident #struct_name #ty_generics
             #where_clause
             {
-                type Id = (#(&'ident #field_ty),*);
+                type Id = (#(&'ident #ref_field_ty),*);
 
                 fn id(self) -> Self::Id {
-                    (#(&self#field_access),*)
+                    (#(&self#ref_field_access),*)
+                }
+            }
+
+            impl #impl_generics Identifiable for #struct_name #ty_generics
+            #where_clause
+            {
+                type Id = (#(#direct_field_ty),*);
+
+                fn id(self) -> Self::Id {
+                    (#(self#direct_field_access),*)
                 }
             }
         },
