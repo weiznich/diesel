e6934426ff9ae2dcd1300001fae148820e25342e
Georg Semmler <georg_semmler_05@web.de>
2018-03-02 15:11:54 +0100

    Revert "Derive `Identifiable` similar to `Insertable` and `AsChangeset`"
    This reverts commit 6bbb9b5bb59ea7fe63891477dfa9e20c2d1e1e36.

--- diesel_derives/src/identifiable.rs
+++ diesel_derives/src/identifiable.rs
@@ -14,14 +14,12 @@ pub fn derive(item: syn::DeriveInput) -> Result<quote::Tokens, Diagnostic> {
     ref_generics.params.push(parse_quote!('ident));
     let (ref_generics, ..) = ref_generics.split_for_impl();
 
-    let (ref_field_ty, ref_field_access): (Vec<_>, Vec<_>) = model
+    let (field_ty, field_access): (Vec<_>, Vec<_>) = model
         .primary_key_names
         .iter()
         .filter_map(|&pk| model.find_column(pk).emit_error())
         .map(|f| (&f.ty, f.name.access()))
         .unzip();
-    let direct_field_ty = ref_field_ty.clone();
-    let direct_field_access = ref_field_access.clone();
 
     Ok(wrap_in_dummy_mod(
         model.dummy_mod_name("identifiable"),
@@ -41,20 +39,10 @@ pub fn derive(item: syn::DeriveInput) -> Result<quote::Tokens, Diagnostic> {
             impl #ref_generics Identifiable for &'ident #struct_name #ty_generics
             #where_clause
             {
-                type Id = (#(&'ident #ref_field_ty),*);
+                type Id = (#(&'ident #field_ty),*);
 
                 fn id(self) -> Self::Id {
-                    (#(&self#ref_field_access),*)
-                }
-            }
-
-            impl #impl_generics Identifiable for #struct_name #ty_generics
-            #where_clause
-            {
-                type Id = (#(#direct_field_ty),*);
-
-                fn id(self) -> Self::Id {
-                    (#(self#direct_field_access),*)
+                    (#(&self#field_access),*)
                 }
             }
         },
